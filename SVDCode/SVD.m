032266bc9050b7e1034ffa72feb65b7b35efaf45
Langyu Qie <70163841+langyu-qie@users.noreply.github.com>
2021-09-22 02:23:39 -0400

    Add files via upload

---
+++ SVDCode/SVD.m
@@ -0,0 +1,37 @@
+
+%This program takes an m-by-n matrix A and computes the SVD decomposition.
+%U and V are m-by-m and n-by-n orthonormal matrices such that A=U*SIGMA*V'
+%for m-by-n zero matrix SIGMA with diagonal consisting of the singular values of
+%A.
+
+
+function [U,SIGMA,V] = SVD(A,p)
+    [m,n] = size(A);
+    dual = 0;   %indicate whether working with A or A'
+    if m > n    %standardizes m>n to m<n
+        A = A';
+        [m,n] = size(A);
+        dual = 1;
+    end
+    [H,Q] = Hessenberg(A'*A);
+    [SIGMA2,G1] = IterateGivens(H,1); %iterations determine accuracy
+    [SIGMA2,G2] = IterateGivensTrunc(SIGMA2,49,p);
+    SIGMA = sqrt(abs(SIGMA2));
+    SIGMAINV = zeros(n);
+    for i = 1:n %construct pseudoinverse of SIGMA
+        if SIGMA(i,i) ~= 0
+            SIGMAINV(i,i) = 1/SIGMA(i,i);
+        end
+    end
+    V = Q'*G1'*G2';
+    U = A*V*SIGMAINV;
+    U = U(1:m,1:m); %trim U to be m-by-m
+    SIGMA = SIGMA(1:m,1:n); %trim SIGMA to be m-by-n
+    if dual == 1 %convert answer from A' to A if swapped
+        [U,V] = deal(V,U);
+    end
+end
+    
+    
+    
+    
